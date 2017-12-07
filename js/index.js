7ea641a9c4bd05e9a6bde7edcb27a13f855f6ce4
Gaku KURIO <gakukurio@Gaku-no-MacBook-Pro.local>
2017-12-07 15:06:18 +0900

    aim test signaling server

--- js/index.js
+++ js/index.js
@@ -26,7 +26,8 @@ var PT_FILE = 102;
 var PT_CAM_BASE = 110;
 var P2P_API_KEY = "v8df88o1y4zbmx6r";
 var PUBLIC_VIEWER = "http://picam360.github.io/picam360-viewer/";
-var SIGNALING_HOST = "peer.picam360.com";
+//var SIGNALING_HOST = "peer.picam360.com";
+var SIGNALING_HOST = "test-peer-server.herokuapp.com";
 var SIGNALING_PORT = 443;
 var SIGNALING_SECURE = true;
 
